56bed2328fc6f575efcf495c00102d70b3648bb9
Thabo Buthelezi <37297137+Spirall@users.noreply.github.com>
2020-04-25 10:01:45 +0200

    deleted java FE code for now, will focus on BE 1st Please enter the commit message for your changes. Lines starting

--- app/shared/interfaces/api-response.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface IAPIResponse1 {
-    req: string;
-    data: any;
-    application_room_no?: string;
-}
--- app/shared/interfaces/routes.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by MarvineBamba on 2017/08/20.
- */
-import { ModuleWithProviders } from '@angular/core';
-
-export interface IRouting {
-  routes: ModuleWithProviders;
-  components: any[];
-}
