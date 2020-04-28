6a4eeda0db8b9545a1b38d9c2d0e3fda6ca0c179
Thabo Buthelezi <37297137+Spirall@users.noreply.github.com>
2020-04-28 20:28:00 +0200

    FE update

--- app/main.py
+++ app/main.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+#app.config['SECRET_KEY'] = '5y78e59wt9d6633s11a78994y3e2c6'
 
 
 @app.route("/")
 def home():
     return render_static("index")
 	
-@app.route("/")
+@app.route("/Contact us")
 def contact_us():
     return render_static("contact_us", title='Contact us')
 
-@app.route("/")
+@app.route("/Accommodations")
 def accommodations():
     return render_static("accommodations", title='Accommodations')
 
-@app.route("/")
+@app.route("/Residence Students'")
 def residence_students():
-    return render_static("residence_students", title='Residence Rtudents')
+    return render_static("residence_students", title='Residence Students')
+	
+@app.route("/Student Register")
+def Student_Register():
+	form = StudentRegistrationForm()
+	return render_static("Student_Register.html", title='Student Register')
+	
+@app.route("/Student Login")
+def Student_Login():
+	form = StudentLoginForm()
+	return render_static("Student_Login.html", title='Student Login')
 
 
 @app.route("/<string:page_name>/")
@@ -26,5 +37,7 @@ def render_static(page_name):
 
 
 # https://stackoverflow.com/questions/419163/what-does-if-name-main-do
+# AppId: 5y78e59wt9d6633s11a78994y3e2c6
+
 if __name__ == "__main__":
     app.run()
